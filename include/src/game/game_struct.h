903c5fd27bd41ab7b29f65436283213c4a6f3a82
Jonathan Emmanuel José <Jonathan@localhost.localdomain>
2020-02-18 13:48:47 +0100

    first display is correct

--- include/src/game/game_struct.h
+++ include/src/game/game_struct.h
@@ -12,4 +12,5 @@ typedef struct g_data
     char **player_map;
     __pid_t my_pid;
     __pid_t opponent_pid;
+    char **attack_map;
 } g_data;
--- src/Game/launch_game.c
+++ src/Game/launch_game.c
@@ -7,6 +7,14 @@
 
 #include "src.h"
 
+void print_game_situation(g_data *game_data)
+{
+    my_putstr("\nmy positions:\n");
+    my_put_map(game_data->player_map);
+    my_putstr("\nenemy's positions:\n");
+    my_put_map(game_data->attack_map);
+}
+
 char **insert_ships_final(char **final_tab, char **positions)
 {
     for (int i = 0; i < 4; i++) {
@@ -16,20 +24,13 @@ char **insert_ships_final(char **final_tab, char **positions)
 
 char **init_map_final(void)
 {
-    char **tab = malloc(sizeof(char *) * 11);
+    char **tab = malloc(sizeof(char *) * 9);
 
-    tab[10] = NULL;
-    for (int i = 0; i < 10; i++) {
-        tab[i] = malloc(sizeof(char) * 18);
-        tab[i][17] = '\0';
-        if (i == 0)
-            my_strcpy(tab[i], " |A B C D E F G H\0");
-        else if (i == 1)
-            my_strcpy(tab[i], "-+---------------\0");
-        else {
-            my_strcpy(tab[i], " |. . . . . . . .\0");
-            tab[i][0] = i + '0' - 1;
-        }
+    tab[8] = NULL;
+    for (int i = 0; i < 8; i++) {
+        tab[i] = malloc(sizeof(char) * 9);
+        tab[i][8] = '\0';
+        my_strcpy(tab[i], "........\0");
     }
     return (tab);
 }
@@ -39,15 +40,18 @@ char **create_map(int fd)
     char **final_tab = init_map_final();
     char *buff = malloc(sizeof(char) * MAX_TO_READ + 1);
 
-    read(fd, buff, MAX_TO_READ + 1);
-    final_tab = insert_ships_final(final_tab, my_str_tab(buff));
-    my_putab(final_tab);
+    if (fd != -10) {
+        read(fd, buff, MAX_TO_READ + 1);
+        final_tab = insert_ships_final(final_tab, my_str_tab(buff));
+    }
     free(buff);
+    return (final_tab);
 }
 
 int navy_game(int fd, g_data *game_data)
 {
     game_data->player_map = create_map(fd);
-    (void)game_data;
+    game_data->attack_map = create_map(-10);
+    print_game_situation(game_data);
     return (0);
 }
